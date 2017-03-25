f793756d413cc61f3a20822546e629030b50f5df
Eric Ebbesen <eebbesen.git@gmail.com>
2017-03-25 17:25:58 -0500

    Test remaining endpoints

--- src/test/java/com/humegatech/marvelapi/client/MarvelAPIClientTest.java
+++ src/test/java/com/humegatech/marvelapi/client/MarvelAPIClientTest.java
@@ -6,38 +6,20 @@ import static org.junit.Assert.assertTrue;
 import java.security.NoSuchAlgorithmException;
 
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class MarvelAPIClientTest {
 	private MarvelAPIClient client;
 
+	@Before
+	public void setUp() {
+		client = new MarvelAPIClient(System.getProperty("MARVEL_KEY"), System.getProperty("MARVEL_SECRET"));
+	}
+
 	@After
 	public void tearDown() {
 		client = null;
-	}
-
-	/*
-	 * Functional test case -- requires actual credentials, really calls the
-	 * Marvel API
-	 */
-	@Test
-	public void testGetCharacters() throws NoSuchAlgorithmException {
-		client = new MarvelAPIClient(System.getProperty("MARVEL_KEY"), System.getProperty("MARVEL_SECRET"));
-		String json = client.getCharacters();
-		System.out.println(json);
-		assertTrue(json, json.contains("name"));
-	}
-	
-	/*
-	 * Functional test case -- requires actual credentials, really calls the
-	 * Marvel API
-	 */
-	@Test
-	public void testGetComics() throws NoSuchAlgorithmException {
-		client = new MarvelAPIClient(System.getProperty("MARVEL_KEY"), System.getProperty("MARVEL_SECRET"));
-		String json = client.getComics();
-		System.out.println(json);
-		assertTrue(json, json.contains("digitalId"));
 	}
 
 	@Test
@@ -46,5 +28,52 @@ public class MarvelAPIClientTest {
 		assertEquals("e1cc8f65af55b2f36af47020aa94f5fe", client.md5("1"));
 		// $ md5 -s 1to8
 		// MD5 ("1to8") = e1cc8f65af55b2f36af47020aa94f5fe
+	}
+
+	/*
+	 * Functional test cases -- requires actual credentials, really calls the
+	 * Marvel API
+	 */
+
+	@Test
+	public void testGetCharacters() throws NoSuchAlgorithmException {
+		String json = client.getCharacters();
+		System.out.println(json);
+		assertTrue(json, json.contains("name"));
+	}
+
+	@Test
+	public void testGetComics() throws NoSuchAlgorithmException {
+		String json = client.getComics();
+		System.out.println(json);
+		assertTrue(json, json.contains("digitalId"));
+	}
+
+	@Test
+	public void testGetCreators() throws NoSuchAlgorithmException {
+		String json = client.getCreators();
+		System.out.println(json);
+		assertTrue(json, json.contains("middleName"));
+	}
+
+	@Test
+	public void testGetEvents() throws NoSuchAlgorithmException {
+		String json = client.getEvents();
+		System.out.println(json);
+		assertTrue(json, json.contains("previous"));
+	}
+
+	@Test
+	public void testGetSeries() throws NoSuchAlgorithmException {
+		String json = client.getSeries();
+		System.out.println(json);
+		assertTrue(json, json.contains("rating"));
+	}
+
+	@Test
+	public void testGetStories() throws NoSuchAlgorithmException {
+		String json = client.getStories();
+		System.out.println(json);
+		assertTrue(json, json.contains("originalIssue"));
 	}
 }
